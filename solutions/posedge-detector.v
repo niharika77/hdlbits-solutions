70f9d9e7ce290a7a788a420fbd4ff1c385b15800
Niharika Mittal <niharikamittal95@gmail.com>
2019-07-04 21:25:06 -0700

    Create posedge-detector.v

---
+++ solutions/posedge-detector.v
@@ -0,0 +1,30 @@
+//For each bit in an 8-bit vector, detect when the input signal changes from 0 in one clock 
+//cycle to 1 the next (similar to positive edge detection). The output bit should be set the cycle 
+//after a 0 to 1 transition occurs.
+
+//************SOLUTION****************//
+//output is set after one clock cycle delay.
+
+module top_module (
+    input clk,
+    input [7:0] in,
+    output [7:0] pedge
+);
+    
+    reg [7:0] temp;
+    wire [7:0] temp_out;
+    
+    always @(posedge clk)
+        begin
+            temp <= in;
+        end
+    
+    assign temp_out = in & ~temp;
+    
+    always @(posedge clk)
+        begin
+            pedge <=temp_out;
+        end
+    
+
+endmodule
